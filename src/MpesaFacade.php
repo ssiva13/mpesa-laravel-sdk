c8d0033157c15ec22b2d1d6c545cb4821d3cf1ac
Simon Siva <simonsiva13@gmail.com>
2023-04-13 17:12:26 +0300

    add service provider and facade classes

---
+++ src/MpesaFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Date 13/04/2023
+ *
+ * @author   Simon Siva <simonsiva13@gmail.com>
+ */
+class MpesaFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'Ssiva\MpesaDaraja\Mpesa';
+    }
+}
---
+++ src/MpesaServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+use Ssiva\MpesaDaraja\Mpesa;
+
+/**
+ * Date 13/04/2023
+ *
+ * @author   Simon Siva <simonsiva13@gmail.com>
+ */
+class MpesaServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/config/mpesa.php' => config_path('mpesa.php')
+        ], 'mpesa_config');
+    }
+    
+    /**
+     * Register the application services.
+     */
+    public function register(): void
+    {
+        $this->app->bind('Ssiva\MpesaDaraja\Mpesa', function ($app) {
+            $config = $app['config']->get('mpesa');
+            return new Mpesa($config);
+        });
+    }
+}
